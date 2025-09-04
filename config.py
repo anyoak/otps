7cc11a35310143466f2d129a8ed6a0a9dba79238
anyoak <anyoakmail@gmail.com>
2025-09-04 12:26:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Telegram Bot settings
+BOT_TOKEN = "8266113070:AAGH3dIo_PJLQHkdQXW8NoY_Z-aDllyAZT4"
+CHAT_ID = "-1002923225320"
+
+# Seven1tel Panel URLs
+LOGIN_URL = "http://94.23.120.156/ints/login"
+SMS_URL   = "http://94.23.120.156/ints/client/SMSCDRStats"
