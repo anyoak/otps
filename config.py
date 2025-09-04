83caab617f87ba03b48e176b9d08edfd4bbd2d84
anyoak <anyoakmail@gmail.com>
2025-09-04 13:29:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-# config.py
-
-# Telegram Bot settings
-BOT_TOKEN = "8266113070:AAGH3dIo_PJLQHkdQXW8NoY_Z-aDllyAZT4"
-CHAT_ID = "-1002923225320"
-
-# Seven1tel Panel URLs
-LOGIN_URL = "http://94.23.120.156/ints/login"
-SMS_URL   = "http://94.23.120.156/ints/client/SMSCDRStats"
+BOT_TOKEN = "8266113070:AAGH3dIo_PJLQHkdQXW8NoY_Z-aDllyAZT4"  # Your Telegram bot token from BotFather
+CHAT_ID = "-1002923225320"                                     # Your Telegram chat ID
+SMS_URL = "http://94.23.120.156/ints/client/SMSCDRStats"       # URL of the SMS page to scrape
+LOGIN_URL = "http://94.23.120.156/ints/login"                  # URL of the login page
+TIMEZONE_OFFSET = 8                                             # For UTC+8 (based on your timezone)
