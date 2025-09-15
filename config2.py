33c5a3099aeafb8b029eb3f0429ebfa9cea48392
anyoak <anyoakmail@gmail.com>
2025-09-16 02:43:49 +0600

    Create config2.py

---
+++ config2.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8212364814:AAEvFHjIrX6J_NCXhuFBtexDYkY_g4Pziug"  # Your Telegram bot token from BotFather
+CHAT_ID = "-1002825191349"                                     # Your Telegram chat ID
+SMS_URL = "https://beta.full-sms.com/stats"       # URL of the SMS page to scrape
+LOGIN_URL = "https://beta.full-sms.com/auth/signin?callbackUrl=https%3A%2F%2Fbeta.full-sms.com"                  # URL of the login page
+TIMEZONE_OFFSET = 8                                             # For UTC+8 (based on your timezone)
