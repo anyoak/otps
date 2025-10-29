a7c313135835ec95ce992a67e94f2a8f79814102
anyoak <anyoakmail@gmail.com>
2025-10-29 06:21:46 +0600

    Create ivasconfig.py

---
+++ ivasconfig.py
@@ -0,0 +1,7 @@
+# config.py
+BOT_TOKEN   = "8335302596:AAFDsN1hRYLvFVawMIrZiJU8o1wpaTBaZIU"
+GROUP_ID    = -1002631004312          # <-- replace with your real group ID
+LOGIN_URL   = "https://www.ivasms.com/login"
+PORTAL_URL  = "https://www.ivasms.com/portal"
+MONITOR_URL = "https://www.ivasms.com/portal/sms/test/sms?app=Telegram"
+LOGIN_TIMEOUT = 300
