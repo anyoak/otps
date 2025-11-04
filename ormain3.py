07639e9bff1bbca79f0f6ff2610666c14a92681b
anyoak <anyoakmail@gmail.com>
2025-11-04 09:18:33 +0200

    Update ormain3.py

--- ormain3.py
+++ ormain3.py
@@ -359,7 +359,7 @@ def main():
             while error_count < config.MAX_ERRORS:
                 try:
                     # Refresh page every 30 minutes to maintain session
-                    if (datetime.now() - last_refresh).total_seconds() > 1800:
+                    if (datetime.now() - last_refresh).total_seconds() > 200:
                         sb.driver.refresh()
                         WebDriverWait(sb.driver, 15).until(EC.presence_of_element_located((By.ID, "LiveCalls")))
                         last_refresh = datetime.now()
