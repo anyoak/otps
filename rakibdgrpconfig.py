1a938e1316624b0275a8d447d09030fe9e9b4453
anyoak <anyoakmail@gmail.com>
2025-11-05 17:55:55 +0100

    Create rakibdgrpconfig.py

---
+++ rakibdgrpconfig.py
@@ -0,0 +1,13 @@
+# Bot Configuration
+BOT_TOKEN = "8371638048:AAEHGvy-vYHmUFPXslg-2toZgOA_14osM9k"
+CHAT_ID = "-1002287664519"
+
+# Website URLs
+BASE_URL = "https://d-group.stats.direct/dashboard"
+LOGIN_URL = "https://d-group.stats.direct/user-management/auth/login"
+SMS_URL = "https://d-group.stats.direct/sms-records/index"
+
+# Settings
+REFRESH_INTERVAL = 5  # seconds
+MAX_MESSAGES_STORE = 2000
+SCROLL_ATTEMPTS = 3
