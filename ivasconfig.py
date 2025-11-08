f9ab8225f5a606ef7fbf54aa5938372e2da64028
anyoak <anyoakmail@gmail.com>
2025-11-08 11:00:27 +0100

    Update ivasconfig.py

--- ivasconfig.py
+++ ivasconfig.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "8335302596:AAFDsN1hRYLvFVawMIrZiJU8o1wpaTBaZIU"
 CHAT_ID = -1002631004312
 LOGIN_URL = "https://www.ivasms.com/login"
-SMS_URL = "https://www.ivasms.com/portal/sms/test/sms?app=Telegram"
+SMS_URL = "https://www.ivasms.com/portal/live/my_sms"
 BASE_URL = "https://www.ivasms.com/portal"
 DOWNLOAD_FOLDER = "downloads"
 MAX_ERRORS = 5
