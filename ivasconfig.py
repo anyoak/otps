68ca2595624abd0009801c7c9a19a49ebfa0bf44
anyoak <anyoakmail@gmail.com>
2025-11-08 11:18:49 +0100

    Update ivasconfig.py

--- ivasconfig.py
+++ ivasconfig.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_TOKEN = "8335302596:AAFDsN1hRYLvFVawMIrZiJU8o1wpaTBaZIU"
-CHAT_ID = -1002631004312
+BOT_TOKEN = "7744467305:AAElMd8DRrtvDcHq9HQvtMTs3G8pO51lsRM"
+CHAT_ID = -1002360271317
 LOGIN_URL = "https://www.ivasms.com/login"
 SMS_URL = "https://www.ivasms.com/portal/live/my_sms"
 BASE_URL = "https://www.ivasms.com/portal"
